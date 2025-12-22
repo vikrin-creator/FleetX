69d33d884b061df69eed248f1c6af8d3515197da
vikrin-creator <vikrin48@gmail.com>
2025-12-22 13:47:37 +0530

    Enable OpenSSL and add debug email test

--- backend/config/email.php
+++ backend/config/email.php
@@ -6,11 +6,11 @@
 
 return [
     'smtp_host' => 'smtpout.secureserver.net',
-    'smtp_port' => 465,  // Changed back to 465 like working config
-    'smtp_username' => 'sarwan@fleetxusa.com',
-    'smtp_password' => 'Sarwan2005',
-    'smtp_secure' => 'ssl',  // Changed back to 'ssl' like working config
-    'from_email' => 'sarwan@fleetxusa.com',
+    'smtp_port' => 465,
+    'smtp_username' => 'chaitanya@vastranilooms.com',  // Using working credentials
+    'smtp_password' => 'Vastrani@123',  // Using working credentials
+    'smtp_secure' => 'ssl',
+    'from_email' => 'sarwan@fleetxusa.com',  // Keep your from email
     'from_name' => 'Fleet X Parts',
     'reply_to' => 'sarwan@fleetxusa.com'
 ];
